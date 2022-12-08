2ba84627dd3480537fe004fef74ecb1019502ff3
Vladimir Chernyy <vladimir_chernyy@list.ru>
2022-12-08 18:15:22 +0400

    add shop in Git

--- myshop/main/urls.py
+++ myshop/main/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('',views.catalog, name='catalog'),
     path('about',views.about, name='about'),
     path('create',views.create, name='create'),
-    path('manual_tile_сutter',views.manual_tile_сutter, name='manual_tile_сutter'),
+    path('manual_tile_сutter',views.manual_tile_cutter, name='manual_tile_сutter'),
     path('electric_tile_cutter',views.electric_tile_cutter, name='electric_tile_cutter'),
     path('XL_size',views.XL_size, name='XL_size'),
 ]
--- myshop/main/views.py
+++ myshop/main/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render, redirect
-from .models import Task
-from .forms import TaskForm
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# all_urls = {
+#     'catalog': 'main/catalog.html',
+#     'about': 'main/about.html',
+#     'create': 'main/create.html',
+#     'manual_tile_сutter': 'main/manual_tile_сutter.html',
+#     'electric_tile_cutter': 'main/electric_tile_cutter.html',
+#     'XL_size': 'main/XL_size.html',
+# }
 
 
 def catalog(request):
-    tasks = Task.objects.all()
-    return render(request, 'main/catalog.html', {'title': 'Каталог', 'tasks': tasks})
+    return render(request, 'main/catalog.html')
 
 
 def about(request):
@@ -13,28 +20,16 @@ def about(request):
 
 
 def create(request):
-    error = ''
-    if request.method == 'POST':
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('catalog')
-        else:
-            error = 'Форма была неверной'
-    form = TaskForm()
-    context = {
-        'form': form,
-        'error': error,
-    }
-    return render(request, 'main/create.html', context)
+    return render(request, 'main/create.html', )
 
 
-def manual_tile_сutter(request):
+def manual_tile_cutter(request):
     return render(request, 'main/manual_tile_сutter.html')
 
 
 def electric_tile_cutter(request):
     return render(request, 'main/electric_tile_cutter.html')
+
 
 def XL_size(request):
     return render(request, 'main/XL_size.html')
